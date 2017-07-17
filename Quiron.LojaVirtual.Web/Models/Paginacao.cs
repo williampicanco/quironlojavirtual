6c6ede7200c6ef6b0b7504e8cbba6996469e6446
William Corrêa Picanço Junior <william.picanco@hotmail.com>
2017-07-17 10:07:52 -0300

    Classes de Paginacao

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; } //qtos intem tem no banco.
+        public int ItensPorPagina { get; set; } //qtos itens tem por página.
+        public int PaginaAtual { get; set; } //qual a página exibida no momento.
+        public int TotalPagina //Qtas páginas terei.
+        {
+            get { return (int) Math.Ceiling((decimal) ItensTotal / ItensPorPagina);}
+        }
+    }
+}
