c6822ec86b4366cbec09eda7396860a9616df0bf
William Corrêa Picanço Junior <william.picanco@hotmail.com>
2017-07-20 23:04:10 -0300

    Criação do Btn Adiconar ao carrinho e CarrinhoController.

---
+++ Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Linq;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class TesteCarrinhoCompras
+    {
+
+        //Teste Adicionar Itens ao Carrinho
+        [TestMethod]
+        public void AdicionarItensAoCarrinho()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 2);
+
+            carrinho.AdicionarItem(produto2,3);
+
+            carrinho.AdicionarItem(produto3, 2);
+
+            //Act
+            ItemCarrinho[] itens = carrinho.ItensCarrinho.ToArray();
+
+            //Assert
+            Assert.AreEqual(itens.Length, 3);//testando se a qde de itens do carrinho é igual a 2.
+
+
+            Assert.AreEqual(itens[0].Produto, produto1);
+
+            Assert.AreEqual(itens[1].Produto, produto2);
+         
+
+        }
+
+        [TestMethod]
+        public void AdicionarProdutoExistenteParaCarrinho()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+            //Produto produto3 = new Produto
+            //{
+            //    ProdutoId = 3,
+            //    Nome = "Teste 3"
+            //};
+
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 1);
+
+            carrinho.AdicionarItem(produto2, 1);
+
+            carrinho.AdicionarItem(produto1, 10);
+
+            //Act
+            ItemCarrinho[] resultado = carrinho.ItensCarrinho
+                .OrderBy(c => c.Produto.ProdutoId).ToArray();
+
+
+            Assert.AreEqual(resultado.Length, 2);
+
+            Assert.AreEqual(resultado[0].Quantidade, 11);
+
+            Assert.AreEqual(resultado[1].Quantidade, 1);
+
+        }
+
+        [TestMethod]
+        public void RemoverItensCarrinho()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 1);
+
+            carrinho.AdicionarItem(produto2, 3);
+
+            carrinho.AdicionarItem(produto3, 5);
+
+            carrinho.AdicionarItem(produto2, 1);
+
+            carrinho.RemoverItem(produto2);
+
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Where(c => c.Produto == produto2).Count(), 0);
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 2);
+
+        }
+
+        [TestMethod]
+        public void CalcularValorTotal()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            //Produto produto3 = new Produto
+            //{
+            //    ProdutoId = 3,
+            //    Nome = "Teste 3"
+            //};
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 3);
+
+            decimal resultado = carrinho.ObterValorTotal();
+
+            Assert.AreEqual(resultado, 450M);
+
+        }
+
+        [TestMethod]
+        public void LimparItensCarrinho()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+
+            //Act
+            carrinho.LimparCarrinho();
+       
+            //Assert
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 0);
+
+        }
+
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,67 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    
+    public class CarrinhoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            //Query pegando no banco.
+            Produto produto = _repositorio.Produtos.
+                FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            //Se o produto for nulo.
+            if(produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            //Como vai ser obtido um carrinho é necessário ter um carrinho 1º.
+            //Cria o carrinho e adiciona uma sessão.
+            Carrinho carrinho = (Carrinho)Session["Carrinho"];
+
+            if(carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho; 
+            }
+
+            return carrinho; //senão já retorna o carrinho já existente.
+
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            //Query pegando no banco.
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if(produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+           
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+
+    }
+}
