3767f33bffa95fb9cb44d9d2e2b4541379b4d274
William Corrêa Picanço Junior <william.picanco@hotmail.com>
2017-07-31 18:44:26 -0300

    Método HttpPost  FecharPedido em CarrinhoController.cs

---
+++ Quiron.LojaVirtual.Dominio/Entidades/EmailConfiguracoes.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class EmailConfiguracoes
+    {
+                   
+        public bool UsarSsl = false;
+        public string ServidorSmtp = "smtp.william.com.br";
+        public int ServidorPorta = 587;
+        public string Usuario = "william";
+        public bool EscreverArquivo = false;
+        public string PastaArquivo = @"c:\envioemail";
+        public string De = "william@quiron.com.br";
+        public string Para = "pedido@quiron.com.br";
+
+    }
+}
