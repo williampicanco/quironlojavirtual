b8f691ea698b97ddc83866dac83ac2212a560eb9
William Corrêa Picanço Junior <william.picanco@hotmail.com>
2017-08-05 02:49:44 -0300

    Criação Área Administrativa e CRUD Controller - View Produtos

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //inicia instanciado o repositorio
+        private ProdutosRepositorio _repositotio;
+
+        public ActionResult Index()
+        {
+            _repositotio = new ProdutosRepositorio();
+            var produtos = _repositotio.Produtos;
+            return View(produtos);
+        }
+    }
+}
